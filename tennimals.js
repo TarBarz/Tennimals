05f005bbf552333dd7b6a2296a1ea701e9ee7d66
StellarSpirit <34275038+StellarSpirit@users.noreply.github.com>
2018-02-13 18:53:24 -0500

    Add files via upload
    Added background image, added second player controlled by arrow keys, changed background colour back to indigo

--- tennimals.js
+++ tennimals.js
@@ -3,8 +3,11 @@ canvas.width = 1280;
 canvas.height = 640;
 var surface = canvas.getContext("2d");
 var player = {x:120, y:128, speed:6};
+var player2 = {x:1160, y:640-128, speed:6};
 var playerSprite = new Image();
-playerSprite.src = "img/tennimalscharplaceholder.png";
+playerSprite.src = "tennimalscharplaceholder.png";
+var player2Sprite = new Image();
+player2Sprite.src = "tennimalscharplaceholder.png";
 var interval;
 
 var upPressed = false;
@@ -12,8 +15,15 @@ var downPressed = false;
 var leftPressed = false;
 var rightPressed = false;
 
+var p2UpPressed = false;
+var p2DownPressed = false;
+var p2LeftPressed = false;
+var p2RightPressed = false;
+
 window.addEventListener("keydown", keyDown);
 window.addEventListener("keyup", keyUp);
+window.addEventListener("keydown", p2KeyDown);
+window.addEventListener("keyup", p2KeyUp);
 
 
 
@@ -27,9 +37,12 @@ function startGame()
 function update()
 {
 	movePlayer();
+	movePlayer2();
 	render();
-	console.log(player.x);
-	console.log(player.y);
+	//console.log(player.x);
+	//console.log(player.y);
+	//console.log(player2.x);
+	//console.log(player2.y);
 }
 
 function movePlayer()
@@ -40,14 +53,27 @@ function movePlayer()
 		player.y += player.speed;
 	if (leftPressed == true && player.x > 0)
 		player.x -= player.speed;
-	if (rightPressed == true && player.x < 640-64)
+	if (rightPressed == true && player.x < 640-64-17)
 		player.x += player.speed;
+}
+
+function movePlayer2()
+{
+	if (p2UpPressed == true && player2.y > 0)
+		player2.y -= player2.speed;
+	if (p2DownPressed == true && player2.y < 640-64)
+		player2.y += player2.speed;
+	if (p2LeftPressed == true && player2.x > 640+18)
+		player2.x -= player2.speed;
+	if (p2RightPressed == true && player2.x < 1280-64)
+		player2.x += player2.speed;
 }
 
 function render()
 {
 	surface.clearRect(0,0,1280,640)
 	surface.drawImage(playerSprite, player.x, player.y);
+	surface.drawImage(player2Sprite, player2.x, player2.y);
 }
 
 function keyDown(event)
@@ -70,6 +96,26 @@ function keyDown(event)
 	} 
 }
 
+function p2KeyDown(event)
+{
+	switch (event.keyCode)
+	{
+
+		case 38:
+			p2UpPressed = true;
+			break;
+		case 40:
+			p2DownPressed = true;
+			break;
+		case 37:
+			p2LeftPressed = true;
+			break;
+		case 39:
+			p2RightPressed = true;
+			break;
+	} 
+}
+
 function keyUp(event)
 {
 	switch (event.keyCode)
@@ -87,4 +133,24 @@ function keyUp(event)
 			rightPressed = false;
 			break;
 	}
+}
+
+function p2KeyUp(event)
+{
+	switch (event.keyCode)
+	{
+
+		case 38:
+			p2UpPressed = false;
+			break;
+		case 40:
+			p2DownPressed = false;
+			break;
+		case 37:
+			p2LeftPressed = false;
+			break;
+		case 39:
+			p2RightPressed = false;
+			break;
+	} 
 }
