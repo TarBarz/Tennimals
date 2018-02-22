5017a7d4795d14e04c1939c63c5796390d64d097
JesseLessard <34274782+JesseLessard@users.noreply.github.com>
2018-02-22 13:36:13 -0500

    New Java File
    Added a new Java File for the Start Menu

---
+++ tennimalsStartMenu.js
@@ -0,0 +1,17 @@
+//Jesse/Nate
+/*
+what we need to do:
+*Pathways to the player select and exit Game(Cridits if wanted)
+*Visuals that reprsent the pathways(canvas images and such) 
+*/
+var Menu;
+var ExitGame;//Varable to Exit Game
+var PlayGame;//Varable to Move to the Player select screen
+var canvas = document.querySelector("canvas"); 
+canvas.width = //TBD;
+canvas.height = //TBD;
+var MenuSurface = canvas.getContext("2d");
+function Menu()
+{
+	
+}
