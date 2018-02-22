e58c13e05e4ec40e35754dfe26cd77181e1e8498
JesseLessard <34274782+JesseLessard@users.noreply.github.com>
2018-02-22 14:01:42 -0500

    update to Tennimals start menu

--- tennimalsStartMenu.js
+++ tennimalsStartMenu.js
@@ -5,13 +5,21 @@ what we need to do:
 *Visuals that reprsent the pathways(canvas images and such) 
 */
 var Menu;
-var ExitGame;//Varable to Exit Game
-var PlayGame;//Varable to Move to the Player select screen
+var ExitGame = false;//Varable to Exit Game
+var PlayGame = false;//Varable to Move to the Player select screen
 var canvas = document.querySelector("canvas"); 
 canvas.width = //TBD;
 canvas.height = //TBD;
 var MenuSurface = canvas.getContext("2d");
+
 function Menu()
 {
-	
+	if (//PlayGame = true)
+	{
+		//move to screen select
+	}
+	if (//ExitGame = true)
+	{
+		//Exit Game and maybe move to credit screen
+	}
 }
