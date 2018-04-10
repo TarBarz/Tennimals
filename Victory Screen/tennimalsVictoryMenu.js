7b8a43ab33685931615fab5aaa737e29dab64494
JonBeeson <36206651+JonBeeson@users.noreply.github.com>
2018-04-10 13:12:21 -0400

    Add files via upload
    much better version from what i had last night. still a few bugs and need to figure out how to get the victory screen to pop up after a player has won

---
+++ Victory Screen/tennimalsVictoryMenu.js
@@ -0,0 +1,52 @@
+//Jesse/Nate
+var canvas = document.getElementById("menuCanvas"); 
+var singlePlayerButton = document.getElementById("singlePlayer");
+var versusButton = document.getElementById("versus");
+var creditsButton = document.getElementById("credits");
+var exitButton = document.getElementById("exitGame");
+var displayTitle = document.getElementById("title");
+var menuMusic = document.getElementById("menuAudio").autoplay;
+canvas.width = 1280;
+canvas.height = 640; 
+menuSurface = canvas.getContext("2d");
+singlePlayerButton.addEventListener("click", function clickSinglePlayer()
+{
+	if (singlePlayerButton.click)
+	{
+		import ("tennimalsAI.js");
+		console.log("Single Player Mode!");
+	}
+});
+
+versusButton.addEventListener("click", function clickVersus()
+{
+	if (versusButton.click)
+	{
+		import ("tennimals.js");
+		console.log("Two Player Mode!");
+	}
+});
+
+creditsButton.addEventListener("click", function clickCredits()
+{
+	if (versusButton.click)
+	{
+		//import ("tennimals.js");
+		console.log("Credits!");
+	}
+});
+
+exitButton.addEventListener("click", function clickExit()
+{
+	if (exitButton.click)
+	{
+		close();
+		console.log("exit game!");
+	}
+});
+function displayMenu()
+{
+	menuSurface.fillRect(0,0,1280,640);
+}
+
+
